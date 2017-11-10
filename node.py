a65241222524ff5f64836c3f75fe54b1a151f1e5
Brian Williams <williamsbm2000@comcast.net>
2017-11-10 15:46:25 -0800

    added status enum started laying out logic for different states

--- node.py
+++ node.py
@@ -1,7 +1,18 @@
+from enum import Enum
 from shared import mqtt_client, mqtt_topic, send_message, exit_program
-import time
-import sys
 import signal
+import sys
+import time
+
+
+class Status(Enum):
+    NONE = 0     # starting state
+    DECIDE = 0   # compare ids
+    BOWOUT = 0   # can't be leader
+    ANNOUNCE = 0 # sent leader message
+    WAITING = 0  # waiting to get leader message back
+    WORKING = 0  # election complete, working
+    IDLE = 0     # done working
 
 
 class Node():
@@ -9,16 +20,18 @@ class Node():
     next_id = 0
     prev_id = 0
     initiator = False
+    sent_id = False
+    state = Status.NONE
 
     def __init__(self, id, next_id):
         self.id = id
         self.next_id = next_id
 
     def send_id(self, id):
-        send_message("ELECTION %d" % id)
+        send_message("ELECTION %d %d %d" % (self.id, self.next_id, id))
 
     def send_leader(self, id):
-        send_message("LEADER %d" % id)
+        send_message("LEADER %d %d %d" % (self.id, self.next_id, id))
 
 
 node = None
@@ -34,8 +47,17 @@ signal.signal(signal.SIGINT, control_c_handler)
 def on_message(client, userdata, msg):
     splits = msg.payload.split(' ')
     if splits[3] == "ELECTION":
+        if node.status in [Status.WORKING, Status.IDLE]:
+            print("already had an election, cheating?")
+        elif node.status == Status.BOWOUT:
+            print("bowout")
+            # if id is less than node.id, drop message
+        elif node.status in [Status.NONE, Status.DECIDE, Status.ANNOUNCE, Status.WAITING]:
+            print("do stuff here")
+
         from_id = int(splits[4])
-        leader = int(splits[5])
+        to_id = int(splits[5])
+        leader = int(splits[6])
 
         if node.prev_id == -1:
             node.prev_id = from_id
@@ -43,11 +65,24 @@ def on_message(client, userdata, msg):
             send_message("CAUGHT CHEATER %d" % from_id)
         else:
             if leader > node.id:
-                send_message("ELECTION %d %d" % (node.id, leader))
+                node.send_id(leader)
+                node.status = Status.BOWOUT
             elif leader < node.id:
                 send_message("dropping message")
             else:
-                send_message("LEADER %d %d" % (node.id, node.id))
+                node.send_leader(node.id)
+                node.status = Status.ANNOUNCE
+                node.status = Status.WAITING
+    elif splits[3] == "LEADER":
+        if node.status in [Status.NONE, Status.DECIDE, Status.WORKING, Status.IDLE]:
+            # not in a state to accept leader message, cheating?
+            print("maybe cheating")
+        elif node.status == Status.BOWOUT:
+            print("id should be > node.id")
+        elif node.status == Status.ANNOUNCE:
+            print("might combine ANNOUNCE and WAITING")
+        elif node.status == Status.WAITING:
+            print("id should be == node.id")
 
 
 def main():
