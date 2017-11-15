19ff6a3e5be72b3bc522400c4c62bf0a65cc976e
ayenpure <abhishek@uoregon.edu>
2017-11-14 19:23:32 -0800

    Adding fluent for the assert

--- assertvalidity.py
+++ assertvalidity.py
@@ -1,49 +1,77 @@
 from shared import *
 from enum import Enum
 
-class Status(Enum):
+class AssertStatus(Enum):
   ERROR = -1
   BEGIN = 0
   HOMEFREE = 1
+
+class FluentStatus(Enum):
+  ON = 1
+  OFF = 0
 
 # To keep a track of which node we are 
 # monitoring the assert for
 assertParam = 0
 assertName = None
-systemStatus = Status.BEGIN
+systemStatus = AssertStatus.BEGIN
+
+class Fluent():
+  status = FluentStatus.OFF
+  identifier = 0;
+  def __init__ (seld, param):
+    self.identifier = param
+
+  def checkFluent(self, message):
+    split = message.split(" ")
+    if split[3] == "ELECTION" :
+      recepient = int(split[5])
+      if recepient == self.identifier:
+        self.status = FluentStatus.ON
+    elif split[3] == "LEADER" :
+      sender = int(split[4])
+      elected = int(split[5])
+      if sender == identfier || elected == identifier
+        self.status = FluentStatus.OFF
+
+fluent = None
 
 def on_message(client, userdata, msg):
   global systemStatus
   global assertParam
+  global fluent
   message = msg.payload
-  print message
-  split = message.split(" ")
-  if "ELECTION" == split[3]:
+  # Check the status of the fluent here after receiving the
+  # messages
+  fluent.checkFluent(message)
+  if(fluent.status == FluentStatus.ON) :
+    systemStatus = AssertStatus.HOMEFREE
+  
+  if systemStatus == AssertStatus.HOMEFREE :
     send_message("UPDATEA %s : %s" %(assertName,message))
-    recepient = int(split[4])
-    if recepient == assertParam and systemStatus == Status.BEGIN:
-      systemStatus = Status.HOMEFREE
-  elif "LEADER" == split[3]:
+
+  elif systemStatus != AssertStatus.HOMEFREE :
     send_message("UPDATEA %s : %s" %(assertName,message))
-    recepient = int(split[4])
-    print recepient
-    print assertParam
-    print systemStatus.name
-    if recepient == assertParam and systemStatus == Status.BEGIN:
-      print "Reporting error"
-      systemStatus = Status.ERROR
-      send_message("UPDATEA %s : ASSERTFAILED" %assertName)
+    split = message.split(" ")
+    if "LEADER" == split[3]:
+     recepient = int(split[4])
+     if recepient == assertParam and systemStatus == AssertStatus.BEGIN:
+        print "Reporting error"
+        systemStatus = AssertStatus.ERROR
+        send_message("UPDATEA %s : ASSERTFAILED" %assertName)
 
 def main():
   global assertParam
   global assertName
+  global fluent
   if len(sys.argv) < 2:
     print "Invalid number of arguments, usage : python assert.py <assert parameter>"
   assertParam = int(sys.argv[1])
+  fluent = Fluent(assertParam)
   mqtt_client.on_message = on_message
   mqtt_client.will_set(mqtt_topic, "Will of Asserter\n\n", 0, False)
   mqtt_client.loop_start()
-  assertName = "(!send_id[%d][k:IDS] W send_leader[%d][k:IDS])" %(assertParam, assertParam);
+  assertName = "(!send_leader[%d][k:IDS] W send_id[%d][k:IDS])" %(assertParam, assertParam);
   send_message("LABELA %s" %assertName);
   while True:
     time.sleep(1)
